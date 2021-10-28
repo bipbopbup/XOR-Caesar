a19b32dbed9dca5cb9643d8c4c4ed6773c8955a0
Eskizo2033 <j.esteban.rincon.m@gmail.com>
2021-10-28 17:38:23 +0200

    A ver, que ya ni me acuerdo de esto

--- Ceasar_bruto.py
+++ Ceasar_bruto.py
@@ -1,18 +1,18 @@
 
 
-print("Ceasar Brute Force")
+#print("Ceasar Brute Force")
 
 
 entrada = open ('textito.txt', 'r')
 salida = open ('salidita.txt','w')
 mensaje = entrada.read()
 
-#abc = [a,]
-mensaje_b = mensaje.swapcase
 
-print(mensaje_b)
+abc = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z]
+ABC = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 
 
+#mensaje_b = mensaje.swapcase
 #longitud = len(mensaje)
 #print(longitud)
 
--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,26 @@
-lista = ["a","B","c","D"]
+lista = ["h","O","l","A"]
 
 longitud = len(lista)
 
 print(longitud)
 
-for i in range(len(lista)):
-    c = lista[i]
-    if c.isupper():
-        print(c.swapcase())
+
+abc = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z]
+ABC = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+
+key = 3
+
+a = " "
+
+for i in lista:
+    if i.isupper():
+        while a != i:
+             
+
+
+for i in lista:
+    if i.isupper():
+        print(i.swapcase())
     else:
-        print(c)
+        print(i)
+
